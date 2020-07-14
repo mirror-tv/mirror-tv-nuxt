e90eaaf2657c746d3da501ebe31492f9fffd04c1
kwhsiung <kaiwenhsiung@gmail.com>
2020-07-14 15:49:40 +0800

    chore: remove unnecessary files and code

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,9 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        mirror-tv-nuxt
-      </h1>
-      <h2 class="subtitle">
-        mirror-tv-nuxt
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <div class="container"></div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
 
 <style>
