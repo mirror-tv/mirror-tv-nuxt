d2ac85c84fb8ccc221acdb7087bf462a75a80600
kwhsiung <kaiwenhsiung@gmail.com>
2020-07-21 17:39:39 +0800

    test: add jest-transform-graphql

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,8 @@ module.exports = {
   moduleFileExtensions: ['js', 'vue', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '.*\\.(vue)$': 'vue-jest',
+    '\\.(gql|graphql)$': 'jest-transform-graphql'
   },
   collectCoverage: true,
   collectCoverageFrom: [
