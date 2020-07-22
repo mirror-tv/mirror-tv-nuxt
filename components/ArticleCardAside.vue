753adee5789d252a8d9a9117e5e9cfe70f8b1c6b
kwhsiung <kaiwenhsiung@gmail.com>
2020-07-22 17:24:38 +0800

    feat(article-card-aside): add features and tests

---
+++ components/ArticleCardAside.vue
@@ -0,0 +1,36 @@
+<template>
+  <a
+    class="article-card"
+    :href="href"
+    target="_blank"
+    rel="noreferrer noopener"
+  >
+    <span class="article-img-wrapper">
+      <img v-lazy="articleImgURL" class="article-img" alt="article-img" />
+    </span>
+    <span :class="['bottom-wrapper__info-wrapper', 'info-wrapper']">
+      <span class="article-title" v-text="articleTitle" />
+    </span>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      default: ''
+    },
+    articleImgURL: {
+      type: String,
+      default: require('~/assets/img/default image-2.jpg')
+    },
+    articleTitle: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ components/__tests__/ArticleCardAside.spec.js
@@ -0,0 +1,37 @@
+import { shallowMount } from '@vue/test-utils'
+import ArticleCardAside from '../ArticleCardAside'
+
+describe('props', function() {
+  test('Should render image src by "articleImgURL" props', async function() {
+    expect.assertions(1)
+    const articleImgURLMock = 'articleImgURLMock'
+    const wrapper = await shallowMount(ArticleCardAside, {
+      propsData: {
+        articleImgURL: articleImgURLMock
+      }
+    })
+    const articleImg = wrapper.find('.article-img')
+    expect(articleImg.attributes()['data-src']).toBe(articleImgURLMock)
+  })
+
+  test('Should render text by "articleTitle" props', function() {
+    const articleTitleMock = 'articleTitleMock'
+    const wrapper = shallowMount(ArticleCardAside, {
+      propsData: {
+        articleTitle: articleTitleMock
+      }
+    })
+    const articleTitle = wrapper.find('.article-title')
+    expect(articleTitle.text()).toBe(articleTitleMock)
+  })
+
+  test('Should render href by "href" props', function() {
+    const hrefMock = 'https://www.google.com'
+    const wrapper = shallowMount(ArticleCardAside, {
+      propsData: {
+        href: hrefMock
+      }
+    })
+    expect(wrapper.attributes().href).toBe(hrefMock)
+  })
+})
