10711bab9ec44c95b0c13661b786ada35c895c6d
kwhsiung <kaiwenhsiung@gmail.com>
2020-09-16 16:38:09 +0800

    feat(search-page): add loadmore button

--- pages/search/_keyword.vue
+++ pages/search/_keyword.vue
@@ -21,6 +21,11 @@
             />
           </li>
         </ol>
+        <ButtonLoadmore
+          v-show="showLoadMoreButton"
+          class="list-latest-wrapper__button-load-more button-load-more"
+          @click.native="handleClickMore"
+        />
       </div>
     </div>
   </section>
@@ -28,14 +33,20 @@
 
 <script>
 import ArticleCard from '~/components/ArticleCard'
+import ButtonLoadmore from '~/components/ButtonLoadmore.vue'
 
 export default {
   components: {
-    ArticleCard
+    ArticleCard,
+    ButtonLoadmore
   },
   async fetch() {
     const query = this.$route.params.keyword
-    const response = await this.$axios.post('/api/search', { query })
+    const response = await this.$axios.post('/api/search', {
+      query,
+      from: 0,
+      size: this.listDataMaxResults
+    })
     this.setListData(response)
     this.setListDataTotal(response)
     this.listDataCurrentPage += 1
@@ -44,8 +55,16 @@ export default {
     return {
       listData: [],
       listDataCurrentPage: 0,
-      listDataMaxResults: 9,
+      listDataMaxResults: 12,
       listDataTotal: undefined
+    }
+  },
+  computed: {
+    showLoadMoreButton() {
+      return (
+        this.listDataMaxResults * (this.listDataCurrentPage + 1) <
+        this.listDataTotal
+      )
     }
   },
   methods: {
@@ -70,6 +89,16 @@ export default {
     },
     setListDataTotal(response = {}) {
       this.listDataTotal = response.data?.body?.hits?.total?.value ?? 0
+    },
+    async handleClickMore() {
+      const query = this.$route.params.keyword
+      const response = await this.$axios.post('/api/search', {
+        query,
+        from: this.listDataMaxResults * this.listDataCurrentPage - 1,
+        size: this.listDataMaxResults
+      })
+      this.setListData(response)
+      this.listDataCurrentPage += 1
     }
   }
 }
@@ -88,7 +117,7 @@ export default {
   padding-top: calc(130px + 13px);
   @include media-breakpoint-up(xl) {
     padding: 30px 0 60px 0;
-    max-width: calc(180px * 4 + 21px * 3);
+    width: calc(180px * 4 + 21px * 3);
     margin: 0 auto;
     flex-direction: row;
   }
@@ -129,6 +158,13 @@ export default {
     @include media-breakpoint-up(xl) {
       margin: 0 0 26px 21px;
     }
+  }
+}
+
+.button-load-more {
+  width: 100%;
+  @include media-breakpoint-up(xl) {
+    width: 300px;
   }
 }
 </style>
