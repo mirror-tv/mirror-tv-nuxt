a4afa95fbd872c8b891d4708fb0978a6f3fbcf28
HY Tan <hsuehyungtan@gmail.com>
2020-12-03 17:47:29 +0800

    feat(author): add author page and contact info on author page

---
+++ components/ContactBlock.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="contact">
+    <div class="left-container">
+      <img :src="image" :alt="name" />
+      <div v-if="hasSocialMedia" class="contact__social-media">
+        <a
+          v-if="facebook"
+          :href="facebook"
+          class="link"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src="~/assets/img/facebook-logo-grey-58.png" alt="Facebook" />
+        </a>
+        <a
+          v-if="instatgram"
+          :href="instatgram"
+          class="link"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src="~/assets/img/IG-logo.svg" alt="Instatgram" />
+        </a>
+      </div>
+    </div>
+    <div class="right-container">
+      <h1 v-if="name" class="contact__name" v-text="name" />
+      <p v-if="profile" class="contact__profile" v-text="profile" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: '',
+    },
+    image: {
+      type: String,
+      default: require('~/assets/img/default image-2.jpg'),
+    },
+    facebook: {
+      type: String,
+      default: '',
+    },
+    instatgram: {
+      type: String,
+      default: '',
+    },
+    profile: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    hasSocialMedia() {
+      return this.facebook || this.instatgram
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.contact {
+  @include media-breakpoint-up(lg) {
+    display: flex;
+    .left-container {
+      width: 100px;
+    }
+    .right-container {
+      flex: 1;
+      margin: 0 0 0 30px;
+    }
+  }
+  img {
+    width: 100px;
+    height: 100px;
+    margin: 0 auto;
+    object-fit: cover;
+    object-position: center center;
+    border-radius: 50%;
+  }
+  &__name {
+    margin: 30px auto 0;
+    font-size: 20px;
+    font-weight: 500;
+    text-align: center;
+    @include media-breakpoint-up(lg) {
+      margin-top: 0;
+      text-align: left;
+    }
+  }
+  &__social-media {
+    display: flex;
+    justify-content: center;
+    margin: 10px auto 0;
+    .link {
+      img {
+        width: 20px;
+        height: 20px;
+        object-fit: contain;
+        object-position: center center;
+      }
+      + .link {
+        margin-left: 9px;
+      }
+    }
+  }
+  &__profile {
+    margin: 10px 0 0;
+    text-align: justify;
+    line-height: 1.75;
+  }
+}
+</style>
---
+++ pages/author/_slug.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="g-page-with-aside">
+    <main class="main">
+      <ContactBlock
+        :name="contact.name"
+        :image="contactImage"
+        :facebook="contact.facebook"
+        :instatgram="contact.instatgram"
+        :profile="contact.bio"
+      />
+    </main>
+    <aside class="g-aside" />
+  </section>
+</template>
+
+<script>
+import { fetchContactBySlug } from '~/apollo/queries/contact.gql'
+import ContactBlock from '~/components/ContactBlock'
+
+export default {
+  apollo: {
+    contact: {
+      query: fetchContactBySlug,
+      variables() {
+        return {
+          slug: this.$route.params.slug,
+        }
+      },
+      update(data) {
+        return data.allContacts?.[0]
+      },
+    },
+  },
+  components: {
+    ContactBlock,
+  },
+  computed: {
+    contactImage() {
+      return this.contact.image?.urlMobileSized
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  @include media-breakpoint-up(lg) {
+    padding-right: 118px;
+  }
+}
+
+.g-aside {
+  @include media-breakpoint-up(lg) {
+    background-color: #e7e7e7;
+  }
+}
+</style>
