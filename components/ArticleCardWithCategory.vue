0261a4214158c1fcc379731553bb4a0ffbb3d87a
HY Tan <hsuehyungtan@gmail.com>
2020-12-04 15:57:40 +0800

    feat(author): add latest posts

---
+++ components/ArticleCardWithCategory.vue
@@ -0,0 +1,130 @@
+<template>
+  <a
+    :href="href"
+    target="_blank"
+    rel="noreferrer noopener"
+    class="article-card"
+  >
+    <img class="article-card__img" :src="articleImgURL" :alt="articleTitle" />
+    <div class="article-card__info-wrapper">
+      <div v-if="hasCategoryOrDate" class="article-card__sub-info">
+        <span class="article-card__category" v-text="articleCategory" />
+        <span class="article-card__date" v-text="articleDate" />
+      </div>
+      <h2 class="article-card__title" v-text="articleTitle" />
+      <p
+        v-if="articleDescription"
+        class="article-card__brief"
+        v-text="articleDescription"
+      />
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      default: '',
+    },
+    articleImgURL: {
+      type: String,
+      default: require('~/assets/img/image-default.png'),
+    },
+    articleTitle: {
+      type: String,
+      default: '',
+    },
+    articleDescription: {
+      type: String,
+      default: '',
+    },
+    articleCategory: {
+      type: String,
+      default: '',
+    },
+    articleDate: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    hasCategoryOrDate() {
+      return this.articleCategory || this.articleDate
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.article-card {
+  display: flex;
+  &__img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    @include default-background-image;
+  }
+  &__info-wrapper {
+    flex: 1;
+    margin: 0 0 0 10px;
+  }
+  &__sub-info,
+  &__brief {
+    display: none;
+  }
+  &__sub-info {
+    @include media-breakpoint-up(lg) {
+      display: flex;
+      + * {
+        margin-top: 5px;
+      }
+    }
+  }
+  &__category {
+    margin: 0 auto 0 0;
+    color: #014db8;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: normal;
+  }
+  &__date {
+    margin: 0 0 0 auto;
+    font-size: 14px;
+    line-height: 1.57;
+  }
+  &__title {
+    display: -webkit-box;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 1.63;
+    text-align: justify;
+    word-wrap: break-word;
+    word-break: break-all;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    @include media-breakpoint-up(lg) {
+      line-height: 1.75;
+      + * {
+        margin-top: 5px;
+      }
+    }
+  }
+  &__brief {
+    color: #9b9b9b;
+    font-size: 14px;
+    line-height: 1.57;
+    text-align: justify;
+    word-wrap: break-word;
+    word-break: break-all;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    @include media-breakpoint-up(lg) {
+      display: -webkit-box;
+    }
+  }
+}
+</style>
--- pages/author/_slug.vue
+++ pages/author/_slug.vue
@@ -8,13 +8,29 @@
         :instatgram="contact.instatgram"
         :profile="contact.bio"
       />
+      <div class="posts-count">所有文章（{{ postsCount }}）</div>
+      <ArticleCardWithCategory
+        v-for="post in posts"
+        :key="post.slug"
+        :href="post.href"
+        :articleTitle="post.title"
+        :articleDescription="post.brief"
+        :articleImgURL="post.heroImage"
+        :articleCategory="post.category"
+        :articleDate="post.publishTime"
+        class="item"
+      />
     </main>
     <aside class="g-aside" />
   </section>
 </template>
 
 <script>
+import dayjs from 'dayjs'
+
 import { fetchContactBySlug } from '~/apollo/queries/contact.gql'
+import { fetchPostsAndCountByAuthorSlug } from '~/apollo/queries/posts.gql'
+import ArticleCardWithCategory from '~/components/ArticleCardWithCategory'
 import ContactBlock from '~/components/ContactBlock'
 
 export default {
@@ -23,20 +39,63 @@ export default {
       query: fetchContactBySlug,
       variables() {
         return {
-          slug: this.$route.params.slug,
+          slug: this.authorSlug,
         }
       },
       update(data) {
         return data.allContacts?.[0]
       },
     },
+    posts: {
+      query: fetchPostsAndCountByAuthorSlug,
+      variables() {
+        return {
+          slug: this.authorSlug,
+          withCount: true,
+        }
+      },
+      update(data) {
+        this.postsCount = data._allPostsMeta.count
+        return data.allPosts?.map?.(this.restructurePostFields)
+      },
+    },
   },
   components: {
+    ArticleCardWithCategory,
     ContactBlock,
   },
+  data() {
+    return {
+      postsCount: 0,
+    }
+  },
   computed: {
+    authorSlug() {
+      return this.$route.params.slug
+    },
     contactImage() {
       return this.contact.image?.urlMobileSized
+    },
+  },
+  methods: {
+    restructurePostFields(item) {
+      let brief = item.brief
+      try {
+        brief = JSON.parse(item.brief)?.html?.replace?.(/<[^>]*>?/gm, '')
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error)
+      }
+      return {
+        style: item.style,
+        title: item.title,
+        href: `/story/${item.slug}`,
+        brief,
+        publishTime: dayjs(item.publishTime).format('YYYY.MM.DD HH:mm'),
+        heroImage: item.heroImage?.urlMobileSized,
+        category: item.categories?.[0]?.title,
+        test: JSON.parse(item.brief),
+      }
     },
   },
 }
@@ -48,11 +107,41 @@ export default {
     flex: 1;
     padding-right: 118px;
   }
+  .item {
+    margin: 20px 0 0;
+    padding: 10px;
+    border: 1px solid #d8d8d8;
+    @include media-breakpoint-up(lg) {
+      padding: 20px;
+    }
+    ::v-deep {
+      .article-card__img {
+        width: 70px;
+        height: 70px;
+        @include media-breakpoint-up(lg) {
+          width: 100px;
+          height: 100px;
+        }
+      }
+      .article-title {
+        color: #000;
+      }
+    }
+  }
 }
 
 .g-aside {
   @include media-breakpoint-up(lg) {
     background-color: #e7e7e7;
+  }
+}
+
+.posts-count {
+  margin: 30px 0 0;
+  font-weight: 500;
+  text-align: center;
+  @include media-breakpoint-up(lg) {
+    margin-top: 45px;
   }
 }
 </style>
