b65a490705beb306cfc0055a539eabbd52bf820d
KJ WEN <kjwen310@gmail.com>
2021-04-15 14:45:18 +0800

    feat: join data and add GA to ombuds page (#74)
    * feat: adjust ui of complain-block in ombuds page
    
    * feat: join data of ombuds biography and set path of links
    
    * feat: add ga/header to ombuds page and adjust gql file which related to allPosts

--- pages/ombuds/index.vue
+++ pages/ombuds/index.vue
@@ -13,19 +13,44 @@
             </div>
             <div class="ombuds__intro__main__wrapper-info">
               <h3>公評人簡介</h3>
-              <p>
-                話須說陸日別，無相可前人過會自動此以水年美調化為不全年加它聽人室
-                入樂賽日，不實之。已主如真平心生是，候大德數畫車一當；黑長時他以
-                型裡速經足起百母代！由下提位會看。關
-              </p>
-              <button>了解更多</button>
+              <p>{{ ombudsIntroduction }}</p>
+              <a
+                href="/story/biography"
+                target="_blank"
+                rel="noreferrer noopener"
+                class="ombuds__intro__main__wrapper-info--btn"
+                @click="sendGaClickEvent('了解更多')"
+              >
+                了解更多
+              </a>
             </div>
           </div>
         </main>
         <aside class="ombuds__intro__aside">
-          <h3>新聞申訴方式</h3>
-          <p>若您對弊社的新聞品質與內容有意見或ooxxvvwwzz</p>
-          <button>我要申訴</button>
+          <div class="ombuds__intro__aside-text">
+            <h3>新聞申訴方式</h3>
+            <p>若您對弊社的新聞品質與內容有意見或ooxxvvwwzz</p>
+          </div>
+          <div class="ombuds__intro__aside-btn">
+            <a
+              href="/story/complaint"
+              target="_blank"
+              rel="noreferrer noopener"
+              class="ombuds__intro__aside-btn--btn1"
+              @click="sendGaClickEvent('我要申訴')"
+            >
+              我要申訴
+            </a>
+            <a
+              href="/story/complaint"
+              target="_blank"
+              rel="noreferrer noopener"
+              class="ombuds__intro__aside-btn--btn2"
+              @click="sendGaClickEvent('如何申訴')"
+            >
+              如何申訴？
+            </a>
+          </div>
         </aside>
       </section>
       <ol class="ombuds__infolist">
@@ -34,7 +59,12 @@
           :key="info.id"
           class="ombuds__infolist__item"
         >
-          <a :href="info.herf" target="_blank" rel="noreferrer noopener">
+          <a
+            :href="info.herf"
+            target="_blank"
+            rel="noreferrer noopener"
+            @click="sendGaClickEvent(info.ga)"
+          >
             <div class="ombuds__infolist__item-icon">
               <img :src="info.img" :alt="info.title" />
             </div>
@@ -50,7 +80,23 @@
 </template>
 
 <script>
+import { SITE_NAME } from '~/constants'
+import { getDomain } from '~/utils/meta'
+import { sendGaEvent } from '~/utils/google-analytics'
+import { fetchPostPublishedBySlug } from '~/apollo/queries/post.gql'
+
 export default {
+  apollo: {
+    postPublished: {
+      query: fetchPostPublishedBySlug,
+      variables() {
+        return {
+          slug: 'biography',
+        }
+      },
+      update: (data) => data.postPublished?.[0],
+    },
+  },
   data() {
     return {
       infoList: [
@@ -59,45 +105,87 @@ export default {
           t1: '關於鏡電視',
           t2: '公評人',
           herf: '/story/biography',
+          ga: '關於鏡電視公評人',
         },
         {
           img: require('~/assets/img/ombuds/icon-p2.svg'),
           t1: '申訴',
           t2: '流程',
           herf: '/story/complaint',
+          ga: '申訴流程',
         },
         {
           img: require('~/assets/img/ombuds/icon-p3.svg'),
           t1: '新聞製作',
           t2: '準則',
           herf: '/story/standards',
+          ga: '新聞製作準則',
         },
         {
           img: require('~/assets/img/ombuds/icon-p4.svg'),
           t1: '影音',
           t2: '紀錄',
           herf: '/ombuds/archive',
+          ga: '影音紀錄',
         },
         {
           img: require('~/assets/img/ombuds/icon-p5.svg'),
           t1: '常見問題',
           t2: 'FAQ',
           herf: '/story/faq',
+          ga: '常見問題FAQ',
         },
         {
           img: require('~/assets/img/ombuds/icon-p6.svg'),
           t1: '季報',
           t2: '年報',
-          herf: 't0001',
+          herf: '',
+          ga: '季報‧年報',
         },
         {
           img: require('~/assets/img/ombuds/icon-p7.svg'),
           t1: '聯絡',
           t2: '我們',
           herf: 'mailto:ombuds@mnews.tw',
+          ga: '聯絡我們',
         },
       ],
     }
+  },
+  head() {
+    const title = `公評人專區 - ${SITE_NAME}`
+    return {
+      title,
+      meta: [
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: `${getDomain()}${this.$route.path}`,
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: title,
+        },
+      ],
+    }
+  },
+  computed: {
+    ombudsIntroduction() {
+      try {
+        const content = JSON.parse(this.postPublished?.contentApiData)
+        return content?.[0].content?.[0]
+          ? content?.[0].content?.[0].slice(0, 89).concat('...')
+          : ''
+      } catch {
+        return ''
+      }
+    },
+  },
+  methods: {
+    sendGaClickEvent(label) {
+      sendGaEvent(this.$ga)('ombuds')('click')(label)
+    },
   },
 }
 </script>
@@ -127,7 +215,7 @@ export default {
       margin-bottom: 60px;
     }
     @include media-breakpoint-up(xl) {
-      max-height: 320px;
+      max-height: 360px;
       margin-bottom: 90px;
     }
     @include media-breakpoint-up(xxl) {
@@ -172,10 +260,14 @@ export default {
   }
   &__intro {
     &__wrapper {
-      margin-bottom: 28px;
+      margin: 0 0 36px;
+      @include media-breakpoint-up(md) {
+        margin: 0 0 29px;
+      }
       @include media-breakpoint-up(xl) {
         display: flex;
         justify-content: space-between;
+        margin: 0 0 99px;
       }
     }
     &__main {
@@ -201,10 +293,13 @@ export default {
           margin-bottom: 27px;
           @include media-breakpoint-up(md) {
             margin-right: 15px;
-            width: 220%;
+            width: 280%;
             height: auto;
             transform: none;
             margin-bottom: 0;
+          }
+          @include media-breakpoint-up(xl) {
+            margin-right: 29px;
           }
           img {
             position: absolute;
@@ -230,9 +325,11 @@ export default {
             @include media-breakpoint-up(md) {
               font-size: 24px;
               margin-bottom: 16px;
+              min-width: 255px;
             }
             @include media-breakpoint-up(xl) {
               margin-bottom: 49px;
+              min-width: 291px;
             }
           }
           p {
@@ -249,7 +346,8 @@ export default {
               margin-bottom: 28px;
             }
           }
-          button {
+          &--btn {
+            display: block;
             width: 100%;
             color: #ffcc01;
             background-color: $color-blue-deep;
@@ -257,9 +355,13 @@ export default {
             font-size: 20px;
             line-height: 28px;
             letter-spacing: 0.5px;
+            text-align: center;
             padding: 15px 0;
             @include media-breakpoint-up(md) {
               padding: 10px 0;
+            }
+            @include media-breakpoint-up(md) {
+              padding: 15px 0;
             }
           }
         }
@@ -268,72 +370,119 @@ export default {
     &__aside {
       width: calc(100% + 32px);
       transform: translateX(-16px);
-      padding: 20px 56px;
       text-align: center;
-      background-color: #444746;
       @include media-breakpoint-up(md) {
         width: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
-        padding: 0 0 0 18px;
         transform: none;
       }
       @include media-breakpoint-up(xl) {
-        width: 220px;
+        width: 207px;
         display: block;
-        padding: 24px 0 0;
-        margin-bottom: auto;
       }
-      h3 {
-        color: #ffcc01;
-        font-size: 20px;
-        font-weight: 400;
-        line-height: 32px;
-        letter-spacing: 0.5px;
-        margin-bottom: 23px;
+      &-text {
+        padding: 20px 0 23px;
+        background-color: #444746;
         @include media-breakpoint-up(md) {
-          font-size: 24px;
-          margin: 0 7px 0 0;
-        }
-        @include media-breakpoint-up(xl) {
-          margin: 0 0 23px 0;
-        }
-      }
-      p {
-        color: #fff;
-        font-weight: 500;
-        font-size: 13px;
-        line-height: 28px;
-        text-align: left;
-        margin-bottom: 23px;
-        padding: 0 10px;
-        @include media-breakpoint-up(md) {
-          font-size: 16px;
-          margin: 0;
-        }
-        @include media-breakpoint-up(xl) {
-          margin-bottom: 25px;
-          padding: 0 14px;
-        }
-      }
-      button {
-        width: 100%;
-        color: $color-blue-deep;
-        background-color: #ffcc01;
-        font-weight: 600;
-        font-size: 20px;
-        line-height: 28px;
-        letter-spacing: 0.5px;
-        padding: 12px 0;
-        @include media-breakpoint-up(md) {
-          width: 107px;
-          margin-left: auto;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 15px 18px;
+          width: 480px;
         }
         @include media-breakpoint-up(xl) {
           width: 100%;
+          height: 198px;
+          display: block;
+          padding: 26px 14px 0;
+        }
+        h3 {
+          color: #ffcc01;
+          font-size: 20px;
+          font-weight: 400;
+          line-height: 32px;
+          letter-spacing: 0.5px;
+          margin-bottom: 23px;
+          @include media-breakpoint-up(md) {
+            font-size: 24px;
+            margin: 0 7px 0 0;
+          }
+          @include media-breakpoint-up(xl) {
+            margin: 0 0 23px 0;
+          }
+        }
+        p {
+          color: #fff;
+          font-weight: 500;
+          font-size: 13px;
+          line-height: 28px;
+          text-align: left;
+          padding: 0 70px;
+          @include media-breakpoint-up(md) {
+            font-size: 12px;
+            padding: 0;
+          }
+          @include media-breakpoint-up(xl) {
+            font-size: 16px;
+          }
+        }
+      }
+      &-btn {
+        display: flex;
+        font-size: 16px;
+        line-height: 22px;
+        letter-spacing: 0.5px;
+        @include media-breakpoint-up(md) {
+          width: 200px;
+          height: 64px;
+          justify-content: space-between;
           margin-left: auto;
-          padding: 7px 0;
+        }
+        @include media-breakpoint-up(xl) {
+          display: block;
+          width: 100%;
+          font-size: 20px;
+          line-height: 28px;
+          margin: 18px 0 0;
+        }
+        &--btn1 {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 50%;
+          font-weight: 600;
+          color: $color-blue-deep;
+          background-color: #ffcc01;
+          padding: 4px 0;
+          @include media-breakpoint-up(md) {
+            width: calc((100% - 16px) * 0.48);
+            padding: 0;
+          }
+          @include media-breakpoint-up(xl) {
+            width: 100%;
+            padding: 4px 0;
+          }
+        }
+        &--btn2 {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 50%;
+          font-weight: 600;
+          color: #ffcc01;
+          background-color: $color-blue-deep;
+          padding: 4px 0;
+          @include media-breakpoint-up(md) {
+            width: calc((100% - 16px) * 0.52);
+            padding: 0;
+          }
+          @include media-breakpoint-up(xl) {
+            width: 100%;
+            padding: 4px 0;
+            margin: 18px 0 0;
+          }
         }
       }
     }
