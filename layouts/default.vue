5f33ba4a6746b2a3c8edb42d1d2c76daa4f49778
KJ WEN <kjwen310@gmail.com>
2021-05-07 11:01:24 +0800

    fix: change overflow problem when toggling header in mobile (#101)

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,11 +29,11 @@ export default {
     function handleClickHamburgerButton(payload) {
       if (payload) {
         const scrollY = window.scrollY
-        document.body.style.position = 'fixed'
+        document.body.style.overflow = 'hidden'
         document.body.style.top = `-${scrollY}px`
       } else {
         const scrollY = document.body.style.top
-        document.body.style.position = ''
+        document.body.style.overflow = ''
         document.body.style.top = ''
         window.scrollTo(0, parseInt(scrollY || '0') * -1)
       }
