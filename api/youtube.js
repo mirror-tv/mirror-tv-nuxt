8264300a9c4e8ba225c588dd1cd5cb8b237cb8f5
LIYI <liyibass@mirrormedia.mg>
2021-05-07 17:00:31 +0800

    feat(youtubeApi): add ttl to specific playlistID (#105)

--- api/youtube.js
+++ api/youtube.js
@@ -6,13 +6,24 @@ const {
   YOUTUBE_API_TIMEOUT,
 } = require('../configs/config.js')
 
+const PLAYLIST_ID_WHICH_NEED_TTL = 'PLT6yxVwBEbi2dWegLu37V63_tP-nI6em_'
+
 module.exports = async function getYoutubeData(req, res) {
   try {
     const apiUrl = `${YOUTUBE_API_PROTOCOL}://${YOUTUBE_API_HOST}${req.url}`
-    const { data = {} } = await axios.get(apiUrl, {
+    const axiosConfig = {
       timeout: YOUTUBE_API_TIMEOUT,
-    })
-    res.send(data)
+    }
+
+    const playlistId = req.query?.playlistId
+    if (playlistId === PLAYLIST_ID_WHICH_NEED_TTL) {
+      axiosConfig.headers = {
+        'Cache-Set-TTL': 300,
+      }
+    }
+
+    const response = await axios.get(apiUrl, axiosConfig)
+    res.send(response.data)
   } catch (err) {
     const status = err?.response?.status ?? 500
     res.setHeader('Cache-Control', 'no-store')
