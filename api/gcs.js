e2bcf4ccc3d410d778268d615ae4e822ab0a483a
KJ WEN <kjwen310@gmail.com>
2021-06-09 16:25:21 +0800

    feat: remove domain-changing functionality for gcs files (#134)

--- api/gcs.js
+++ api/gcs.js
@@ -1,12 +1,12 @@
 const axios = require('axios')
-const { ENV } = require('../configs/config')
-let DOMAIN = 'dev.mnews.tw'
+// const { ENV } = require('../configs/config')
+const DOMAIN = 'dev.mnews.tw'
 
-if (ENV === 'prod') {
-  DOMAIN = 'statics.mnews.tw'
-} else if (ENV === 'stag') {
-  DOMAIN = 'www-stag.mnews.tw'
-}
+// if (ENV === 'prod') {
+//   DOMAIN = 'statics.mnews.tw'
+// } else if (ENV === 'stag') {
+//   DOMAIN = 'www-stag.mnews.tw'
+// }
 
 module.exports = async function getHeaderData(req, res) {
   try {
